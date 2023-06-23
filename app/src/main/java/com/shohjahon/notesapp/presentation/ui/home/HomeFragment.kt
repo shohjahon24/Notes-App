7f20a74406132cdd794de02c2b3cbdd0cbf2e5e0
shohjahon24 <lalkalaka10@gmail.com>
2023-06-24 02:09:38 +0500

    fix bugs

--- app/src/main/java/com/shohjahon/notesapp/presentation/ui/home/HomeFragment.kt
+++ app/src/main/java/com/shohjahon/notesapp/presentation/ui/home/HomeFragment.kt
@@ -2,7 +2,6 @@ package com.shohjahon.notesapp.presentation.ui.home
 
 import android.os.Bundle
 import android.text.Editable
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
